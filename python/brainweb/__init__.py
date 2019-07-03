ffc4fabeaaa3ebb599d761dd1ee8e905bf063916
Casper da Costa-Luis <imaging@caspersci.uk.to>
2019-07-03 21:56:04 +0100

    fix install

--- python/brainweb/__init__.py
+++ python/brainweb/__init__.py
@@ -2,4 +2,4 @@ from .utils import *
 from .utils import __all__
 __author__ = "Casper O. da Costa-Luis <casper.dcl@physics.org>"
 __licence__ = __license__ = "[MPLv2.0](https://www.mozilla.org/MPL/2.0)"
-__version__ = "0.3.0"
+__version__ = "0.3.1"
